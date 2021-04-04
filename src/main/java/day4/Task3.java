65427c486e2e151b80ef9a4889fb22e52eb36980
Olga Makeeva <oljamackeewa@gmail.com>
2021-04-04 20:42:23 +0300

    korrect days 4 task 3

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -7,21 +7,33 @@ public class Task3 {
     public static void main(String[] args) {
         int[][] matrice = new int[12][8];
         int sum = 0;
+        int[] plus = new int[12];
+        int max = Integer.MIN_VALUE;
+        int index = -1;
         Random random = new Random();
-        for(int i = 0; i < matrice.length; i++){
-            for(int j = 0; j< matrice[i].length; j++){
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice[i].length; j++) {
                 matrice[i][j] = random.nextInt(50);
-                System.out.print(Arrays.deepToString(matrice));
             }
-            System.out.println();
         }
+        System.out.print(Arrays.deepToString(matrice));
+        System.out.println();
 
-        for(int i = 0; i < matrice.length; i++){
-            for(int j = 0; j< matrice[i].length; j++){
+        for (int i = 0; i < matrice.length; ++i) {
+            for (int j = 0; j < matrice[i].length; j++) {
                 sum = sum + matrice[i][j];
+                //plus[j] = sum;
             }
-            System.out.println(sum + " ");
+            plus[i] = sum;
             sum = 0;
         }
+        for (int i = 0; i < plus.length; i++) {
+            if (plus[i] > max) {
+                max = plus[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
     }
 }
+
