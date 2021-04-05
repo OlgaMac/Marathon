c8fe3936f3a2524a156797839a5dbeb558ee8681
Olga Makeeva <oljamackeewa@gmail.com>
2021-04-05 20:11:23 +0300

    added days 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,11 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        StringBuilder sb = new StringBuilder();
+        int size = 20001;
+        for (int i = 1; i < size; i++) {
+            sb.append(i + " ");
+        }
+        System.out.print(sb);
     }
 }
