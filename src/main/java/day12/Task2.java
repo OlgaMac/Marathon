1b931bd46d1356dd81d860c6be1d03bcf05cfdfa
Olga Makeeva <oljamackeewa@gmail.com>
2021-04-14 21:26:53 +0300

    task 2 days 12

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -6,14 +6,12 @@ import java.util.List;
 public class Task2 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < 30; i++ ){
-            if(i%2 == 0){
+        for (int i = 0, j = 300; i < 30 && j < 350; i++, j++) {
+            if (i % 2 == 0) {
                 list.add(i);
             }
-        }
-        for(int i = 300; i < 350; i++ ){
-            if(i%2 != 0){
-                list.add(i);
+            if (j % 2 != 0) {
+                list.add(j);
             }
         }
         System.out.println(list);
