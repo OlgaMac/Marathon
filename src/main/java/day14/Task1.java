c6e725b873d13df7d5044a0eccc4a173fce44255
Olga Makeeva <oljamackeewa@gmail.com>
2021-04-18 12:10:33 +0300

    added days 14

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,36 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File num = new File("file.txt");
+        printSumDigits(num);
+    }
 
+    private static void printSumDigits(File num) {
+        int sum = 0;
+        try {
+            Scanner scanner = new Scanner(num);
+
+            String line = scanner.nextLine();
+            String[] numbersString = line.split(" ");
+            for (String number : numbersString)
+                sum += Integer.parseInt(number);
+
+            System.out.println(sum);
+            if (numbersString.length != 10) {
+                try {
+                    throw new IOException();
+                } catch (IOException e) {
+                    System.out.println("Некорректный входной файл");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,52 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        parseFileToStringList();
+    }
 
+    public static List<People> parseFileToStringList() {
+        File file = new File("people.txt");
+        List<People> people = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line == null) {
+                    break;
+                }
+                String[] cells = line.split(" ");
+
+                People p = new People();
+                p.name = cells[0];
+                p.age = Integer.parseInt(cells[1]);
+                if(p.age < 0){
+                    throw new IllegalAccessException();
+                }
+                people.add(p);
+            }
+            System.out.println(people);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalAccessException e) {
+            System.out.println("Некорректный входной файл");
+        }
+        return people;
+    }
+
+    public static class People {
+        String name;
+        int age;
+
+        @Override
+        public String toString() {
+            return name + " " + age;
+        }
     }
 }
