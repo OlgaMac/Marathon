08f224142b05acb15ec3d9cee29185abe8ca7235
Olga Makeeva <oljamackeewa@gmail.com>
2021-04-24 18:21:17 +0300

    added days 17

---
+++ src/main/java/day17/ChessBoard.java
@@ -0,0 +1,18 @@
+package day17;
+
+public class ChessBoard {
+    private ChessPiece[][] arr;
+
+    public ChessBoard(ChessPiece[][] arr) {
+        this.arr = arr;
+    }
+
+    public void print() {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j].getDesignationFigur());
+            }
+            System.out.println();
+        }
+    }
+}
